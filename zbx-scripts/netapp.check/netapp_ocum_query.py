20e9a546acdfe60d24cca92f7321c043c40ae573
Daniil Ovsyannikov <to.djet@gmail.com>
2018-10-01 17:00:42 +0300

    ~netapp.check

--- zbx-scripts/netapp.check/netapp_ocum_query.py
+++ zbx-scripts/netapp.check/netapp_ocum_query.py
@@ -21,51 +21,67 @@ class OCUM_API(object):
         self.api_uri    = 'https://%s/rest/' % self.address
         self.headers    = {"Accept": "application/vnd.netapp.object.inventory.hal+json"}
 
-
-    #def aggregates(self, query=None, search=None, sort=None, filter=None):
-    def aggregates(self, params=None, discovery=False):
-        resource = sys._getframe().f_code.co_name
+    def items(self, item_type, params=None, discovery=None):
+        resource = item_type + "s"
         resource_uri = self.api_uri + resource
         response = requests.get (url=resource_uri, params=params, headers=self.headers, timeout=30, auth=self.creds, verify=False)
-        
-        if response and '_embedded' in response.json().keys():
-            output = aggregates = response.json()['_embedded']['netapp:aggregateInventoryList']
+        output = []
+        if response:
+            if '_embedded' in response.json().keys():
+                output = response.json()['_embedded']['netapp:{}InventoryList'.format(item_type)]
 
             if discovery:
                 data = []
         
-                for aggr in aggregates:
-                    data.append( {
-                            '{#AGGR_NAME}'   : aggr['aggregate']['label'],
-                            '{#AGGR_ID}'     : aggr['aggregate']['id'],
-                            '{#NODE_NAME}'   : aggr['node']['label'],
-                            '{#NODE_ID}'     : aggr['node']['id'],
-                            '{#CLUS_NAME}'   : aggr['cluster']['label'],
-                            '{#CLUS_ID}'     : aggr['cluster']['id'],
-                            '{#STATUS}'      : aggr['status']
+                for item in output:
+                    discovery_item = []
+                    if item_type == "aggregate":
+                        discovery_item = {
+                            '{#AGGR_NAME}'   : item['aggregate']['label'],
+                            '{#AGGR_ID}'     : item['aggregate']['id'],
+                            '{#NODE_NAME}'   : item['node']['label'],
+                            '{#NODE_ID}'     : item['node']['id'],
+                            '{#CLUS_NAME}'   : item['cluster']['label'],
+                            '{#CLUS_ID}'     : item['cluster']['id'],
+                            '{#STATUS}'      : item['status']
+                        }
 
-                        })
+                    if item_type == "svm":
+                        discovery_item = {
+                            '{#SVM_NAME}'   : item['svm']['label'],
+                            '{#SVM_ID}'     : item['svm']['id'],
+                            '{#CLUS_NAME}'   : item['cluster']['label'],
+                            '{#CLUS_ID}'     : item['cluster']['id'],
+                            '{#STATUS}'      : item['status']
+                        }
+
+
+                    if discovery_item: data.append(discovery_item)
 
                 output = {'data': data}
 
-            return output
-
         else:
             logging.error('Incorrect response ({}):\n{}'.format(response.status_code, response.text))
-            return False
-            #exit()
+
+        return output
+
+    # def volume(self, params=None, discovery=False):
+    #     item_type = sys._getframe().f_code.co_name
+    #     items = self.items(item_type=item_type, params=params)
+    #     return items
     
+
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--ocum-addr", help="OCUM server address")
     parser.add_argument("--ocum-user", help="OCUM account login")
     parser.add_argument("--ocum-pass", help="OCUM account password")
-    parser.add_argument("--dry-run", action="store_true", help="turns on dry run mode (doesn't modify data)")
-    parser.add_argument("-d", "--debug", action="store_true", help="turns on debug mode")
-    parser.add_argument("--trace", action="store_true", help="turns on trace mode")
-    parser.add_argument("--query", help="Query type: <aggregates|volumes>")
-    parser.add_argument("--discovery", action="store_true", help="Discovery mode")
+    parser.add_argument("--dry-run", action="store_true", help="dry run mode")
+    parser.add_argument("-d", "--debug", action="store_true", help="debug mode")
+    parser.add_argument("--trace", action="store_true", help="trace mode")
+    parser.add_argument("--query", help="Query type: <aggregate|volume|etc>")
+    parser.add_argument("--discovery", action="store_true", help="Output in Zabbix Discovery format")
     parser.add_argument("--params", help="Query params (json)")
 
     args = parser.parse_args()
@@ -74,21 +90,21 @@ def main():
     DRYRUN              = debug_toolkit.DRYRUN = args.dry_run
     
     OCUM_ADDR           = args.ocum_addr
-    OCUM_CREDS          = (args.ocum_user, args.ocum_pass)
+    OCUM_CRED           = (args.ocum_user, args.ocum_pass)
 
-    ocum = OCUM_API(OCUM_ADDR, OCUM_CREDS)
-     
+    ocum = OCUM_API(OCUM_ADDR, OCUM_CRED)
     
-    try:
-        params = json.loads(args.params)
-    except:
-        params = None   #{'nodeId': 8}
-        logging.warning("params invalid: {}".format(args.params))
+    params = None   #{'nodeId': 8}
+    if args.params:
+        try:
+            params = json.loads(args.params)
+        except:
+            logging.warning("params invalid: {}".format(args.params))
 
-    result = getattr(ocum, args.query)(params = params, discovery = args.discovery)
-    result = json.dumps(result)
+    items = ocum.items(item_type=args.query, params=params, discovery=args.discovery)
+    result = json.dumps(items)
+    #result = getattr(ocum, args.query)(params = params, discovery = args.discovery)
     if result: print(result)
-
 
 if __name__ == "__main__":
     main()
